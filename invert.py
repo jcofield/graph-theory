79ba8b78e90d45e768189f039d94120640be72d5
jcofield <joelcofield@gmail.com>
2015-01-29 00:46:14 -0500

    non-recursive function

--- invert.py
+++ invert.py
@@ -1,18 +1,29 @@
 from __future__ import print_function
 
-def invertPath(color1,color2,start,uninvertedG,edgeColor):
-    for vertex in uninvertedG[start]:
-        if edgeColor[start,vertex] in [color1,color2]:
-            if edgeColor[start,vertex] == color1: edgeColor[start,vertex] = color2
-            if edgeColor[start,vertex] == color2: edgeColor[start,vertex] = color1
-            uninvertedG[start].remove(vertex)
-            uninvertedG[vertex].remove(start)
-            edgeColor,uninvertedG = invertPath(color1,color2,vertex,uninvertedG,edgeColor)
-    return edgeColor,uninvertedG
+def invertPath(color1,color2,start,coloredG,edgeColor):
+    def switchColors(vertex1,vertex2):
+        if edgeColor[(vertex1,vertex2)] == color1: newColor = color2
+        elif edgeColor[(vertex1,vertex2)] == color2: newColor = color1
+        edgeColor[(vertex1,vertex2)] = newColor
+        edgeColor[(vertex2,vertex1)] = newColor
+    currentVertex = start
+    lastVertex = start
+    isMorePath = True
+    while isMorePath:
+        isMorePath = False
+        for tryVertex in [v for v in coloredG[currentVertex] if v!=lastVertex]:
+            if edgeColor[currentVertex,tryVertex] in [color1,color2]:
+                switchColors(currentVertex,tryVertex)
+                isMorePath = True
+                lastVertex = currentVertex
+                currentVertex = tryVertex
+                break
 
 #Test
 coloredG = {0:[2,3,4],1:[100,101],2:[0,200,201],3:[0,300,301],4:[0,400,401],100:[1],200:[2],300:[3],400:[4]}
 color = {(1,100):"Red",(1,101):"Green",(0,2):"Blue",(2,200):"Red",(2,201):"Purple",(0,3):"Purple",(3,300):"Red",(3,301):"Green",(0,4):"Red",(4,400):"Green",(4,401):"Purple"}
+for key in color.keys():
+    color[(key[1],key[0])] = color[key]
 print(color)
-color = invertPath("Red","Green",0,coloredG,color)
-print(color)
+invertPath("Red","Green",0,coloredG,color)
+print(color) #the edges (0,4) (4,0) (4,400) and (400,4) should have been switched
